50c9c99ff18a0b0a3bfc736c5d55b288cb88360a
Tumpeti <petertumpek20@gmail.com>
2022-11-26 09:42:25 +0100

    Számológép

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Klónozott verzió")
+# A program írja ki, hogy egy előzetesen megadott szövegben: elkelkáposztásítottalanítottátok hányszor szerepel
+# az e betű! A feladatot ne szövegfüggvénnyel, hanem ciklussal old meg!
+# A rendelkezésre álló idő 10 perc. 
+# A kódról és a program futásáról is képernyőképet kérek feltölteni.
+
+
+"""
+i = 0
+e_betuk = "elkelkáposztásítottalanítottátok"
+e_betuk_szama = 0
+while i < len(e_betuk):
+    if e_betuk[i] == "e":
+        e_betuk_szama += 1
+    i += 1
+print(e_betuk_szama)
+"""
+
+import szamologep
+# szamologep.osszeadas()
+# szamologep.kivonas()
+szamologep.szamologep()
+
+
