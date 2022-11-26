3255c0853bbad51958425f27e727eef2bb81c91b
Tumpeti <petertumpek20@gmail.com>
2022-11-26 10:35:33 +0100

    Globális változó (pl.:szam) és lokális, csak függvényen belül létező változók (pl.:szam1 a fv2-ben) szemléltetése.

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -1,8 +1,13 @@
+szam = 2
 def fv1():
-    szam = 3
+    szam1 = 3
+    print(f"fv1 {szam1}")
+    print(f"fv1 {szam2}")
     print(f"fv1 {szam}")
 
 
 def fv2():
-    szam = 4
-    print(f"fv2 {szam}")
+    szam2 = 4
+    print(f"fv2 {szam1}")
+    print(f"fv1 {szam2}")
+    print(f"fv1 {szam}")
