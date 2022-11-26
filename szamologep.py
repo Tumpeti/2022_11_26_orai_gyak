cf1a55236ebbb8f7a98800d57c4bb77280de73c9
Tumpeti <petertumpek20@gmail.com>
2022-11-26 09:44:15 +0100

    Számológép

---
+++ szamologep.py
@@ -0,0 +1,62 @@
+def osszeadas():
+    # adatbekérés
+    szam1 = float(input("Az első szám: "))
+    szam2 = float(input("Az második szám: "))
+    osszeg = szam1 + szam2
+    # kiírás
+    print(f"{szam1} + {szam2} = {osszeg}")
+    print("-" * 20)
+
+
+def kivonas():
+    # adatbekérés
+    szam1 = float(input("Az első szám: "))
+    szam2 = float(input("Az második szám: "))
+    osszeg = szam1 - szam2
+    # kiírás
+    print(f"{szam1} - {szam2} = {osszeg}")
+    print("-" * 20)
+
+
+def szamologep():
+    # adatbekérés
+    #lokális változó
+    print("-" * 20)
+    szam1 = float(adatbekeres("Adj meg egy számot!"))
+    print("-" * 20)
+    muvjel = adatbekeres("Add meg a műveleti jelet!")
+    print("-" * 20)
+    szam2 = float(adatbekeres("Adj meg még egy számot!"))
+    print("-" * 20)
+    # számolas
+    szoveg = ""
+    eredmeny = 0
+    if muvjel == "+":
+        eredmeny = szam1 + szam2
+    elif muvjel == "-":
+        eredmeny = szam1 - szam2
+    elif muvjel == "/":
+        eredmeny = szam1 / szam2
+    elif muvjel == "*":
+        eredmeny = szam1 * szam2
+    else:
+        szoveg = "nem értelmezhető a művelet"
+    kiiras()
+
+
+
+
+def adatbekeres(szoveg):
+    print("-" * 20)
+    muvjel = (input(szoveg))
+    return muvjel   # visszatérési érték
+
+
+def kiiras():
+    # kiírás
+    print("-" * 20)
+    if szoveg == "":
+        print(f"{szam1}{muvjel}{szam2}={eredmeny}")
+    else:
+        print(szoveg)
+    print("-" * 20)
